d3ee8783fa68f9fc46f157a0718f0fe865b57e5f
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2011-05-30 09:14:15 +0900

    #each: The implementation start of the method

--- lib/status_enumerator.rb
+++ lib/status_enumerator.rb
@@ -3,4 +3,11 @@ class StatusEnumerator
     raise ArgumentError, '%s is not redpond to #each' % target.class.name if target.nil? or !target.respond_to?(:each)
     @target = target
   end
+  
+  def each
+    raise ArgumentError, 'no block given' unless block_given?
+    @target.each do |i|
+      yield
+    end
+  end
 end
--- spec/status_enumerator_spec.rb
+++ spec/status_enumerator_spec.rb
@@ -23,4 +23,23 @@ describe StatusEnumerator do
       lambda { StatusEnumerator.new(nil) }.should raise_error(ArgumentError)
     end
   end
+  
+  describe '#each' do
+    it 'need block given' do
+      lambda { StatusEnumerator.new([]).each }.should raise_error(ArgumentError)
+    end
+
+    it 'not carry out blocking when there is not an element' do
+      i = 0
+      StatusEnumerator.new([]).each { i += 1 }
+      i.should == 0
+    end
+
+    it 'only a number same as an element carries out blocking' do
+      i = 0
+      obj = [2648, 'sample', nil, :test, /aaa/]
+      StatusEnumerator.new(obj).each { i += 1 }
+      i.should == obj.size
+    end
+  end
 end
