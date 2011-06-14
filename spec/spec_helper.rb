76147c428a086ec7637c63391dac48d594014064
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2011-06-14 18:22:19 +0900

    imprement: StatusEnumerator::Status#parent / #each_ancestor with no ancestors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,24 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 RSpec.configure do |config|
   
 end
+
+class Foo
+  def initialize(value, *args)
+    @value, @children = value, args
+  end
+  attr_reader :value, :children
+  
+  def self.conv(ary, &block)
+    ret = ary.map {|x| x.kind_of?(self) ? x.value : x }
+    ret = block.call(ret) if block
+    ary.each_with_index do |obj, i|
+      if obj.kind_of?(self)
+        o = conv(obj.children, &block)
+        o.unshift ret[i]
+        ret[i] = o
+      end
+    end
+    ret.flatten!
+    ret
+  end
+end
