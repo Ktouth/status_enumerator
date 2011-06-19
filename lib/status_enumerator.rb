17e54098c7513f08affaff0994da5992a082846c
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2011-06-19 16:30:40 +0900

    remove: require 'forwardable'

--- lib/status_enumerator.rb
+++ lib/status_enumerator.rb
@@ -1,5 +1,3 @@
-require 'forwardable'
-
 class StatusEnumerator
   def initialize(target)
     raise ArgumentError, '%s is not redpond to #each' % target.class.name if target.nil? or !target.respond_to?(:each)
