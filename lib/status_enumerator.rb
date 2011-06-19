1850217ce395a61aadfc55a930d78300c99dc94e
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2011-06-19 16:25:16 +0900

    refactoring: changed it in form to transfer method StatusEnumerator#each in method #each_status of StatusEnumerator::Status

--- lib/status_enumerator.rb
+++ lib/status_enumerator.rb
@@ -5,48 +5,10 @@ class StatusEnumerator
     raise ArgumentError, '%s is not redpond to #each' % target.class.name if target.nil? or !target.respond_to?(:each)
     @target = target
   end
-  
-  def each
-    raise ArgumentError, 'no block given' unless block_given?
-    c = 0
-    @target.each do |i|
-      put_next i
-      c += 1
-      if c == 3
-        n = put_prev
-        set_flags true, false
-        yield element_status
-        put_next n
-        yield element_status
-      elsif c > 2
-        yield element_status
-      end
-    end.tap do
-      if c > 0
-        case c
-        when 1
-          put_next
-          set_flags true, true
-          yield element_status
-        when 2
-          set_flags true, false
-          yield element_status
-          put_next
-          set_flags false, true
-          yield element_status
-        else
-          put_next
-          set_flags false, true
-          yield element_status
-        end
-      end
-    end
-  end
 
-  extend Forwardable
-  def element_status; @status ||= Status.send(:new, nil) { } end
-  def_delegators :element_status, :put_next, :put_prev, :set_flags
-  private :put_next, :put_prev, :set_flags
+  def each(&block)
+    Status.send(:new, nil, &block).send(:each_status, @target)
+  end
 
   class Status
     class <<self
